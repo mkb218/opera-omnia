82812e2ce012d2beac091f0360e14cf6a83f04e4
Matt Kane <mkb-prime@hydrogenproject.com>
2012-12-19 13:00:25 -0500

    Checkpoint. Issue #28

--- omnia/types.go
+++ omnia/types.go
@@ -1,110 +1,127 @@
 package omnia
 
+import "fmt"
 import "encoding/json"
+import "log"
 import "sync"
 
 // this is a raw audio file that has been uploaded. use libsox to determine its type and re-encode
 // name comes from the form header
 type AudioFile struct {
-	data []byte
-	name string
+	Data []byte
+	Name string
+	Corpus bool
+	Request bool
+	InfoUrl string
+}
+
+func (af AudioFile) MarshalJSON() ([]byte, error) {
+	return nil, nil
 }
 
 // an audio file not (by itself) bound for the corpus. This can be used to create audio from the corpus
 type PostAudioFile struct {
-	trackId string
-	tatums []Tatum
+	TrackId string
+	InfoUrl string
+	Tatums []Tatum
+}
+
+func (paf PostAudioFile) MarshalJSON() ([]byte, error) {
+	return nil, nil
 }
 
 // an audio file bound for (or pulled out of) the corpus
 type CorpusAudioFile struct {
-	trackId string
-	fetchUrl string
-	needData bool
-	tatums []AudioTatum
+	TrackId string
+	InfoUrl string
+	Tatums []AudioTatum
+}
+
+func (caf CorpusAudioFile) MarshalJSON() ([]byte, error) {
+	return nil, nil
 }
 
 // a tatum. This does not, by itself, belong in the corpus.
 type Tatum struct {
-	chroma, timbre []float64
-	loudness_max float64
+	Chroma, Timbre []float64
+	LoudnessMax float64
+	Tstart, Tend float64
 }
 
 // an audiotatum. this belongs in the corpus.
 // data is mp3-encoded, 64kbps, mono
 type AudioTatum struct {
 	Tatum
-	data []byte
-	trackId string
+	Data []byte
+	TrackId string
 }
 
-// type erasure :(
-type queueNode struct {
-	data json.Marshaler
-	next *queueNode
+type ClosedError struct {
+	*Queue
+}
+
+func (c ClosedError) Error() string {
+	return fmt.Sprintf("%p already closed", c.Queue)
+}
+
+type inq struct {
+	m json.Marshaler
+	c chan struct{}
 }
 
 // A Queue is like a buffered channel, with no maximum size
 type Queue struct {
-	head, tail *queueNode
-	push, pop chan json.Marshaler
-	sync.Mutex
+	list []json.Marshaler
+	pop chan json.Marshaler
+	push chan inq
 	length chan int
+	closed bool
+	sync.RWMutex
 }
 
-func NewQueue() *Queue {
+func init() {
+	log.SetFlags(log.LstdFlags|log.Lshortfile)
+}
+
+func (a *Queue) appendToList(in inq) {
+	a.list = append(a.list, in.m)
+	log.Println("appended", a.list)
+	if in.c != nil {
+		in.c <- struct{}{}
+		log.Println("signaled")
+		close(in.c)
+	}
+}
+
+func NewQueue(bufsize int) *Queue {
 	a := new(Queue)
-	a.push = make(chan json.Marshaler)
-	a.pop = make(chan json.Marshaler)
-	a.length = make(chan int)
+	a.push = make(chan inq, bufsize)
+	a.pop = make(chan json.Marshaler, bufsize)
 	go func() {
-		OUTER: for {
-			select {
-			case a.length <- 0:
-			default:
+		for {
+			in, ok := <-a.push
+			// log.Println("in",in,"ok",ok)
+			if ok {
+				a.appendToList(in)
+			} else {
+				goto DRAIN
 			}
-			a.Lock()
-			f, ok := <- a.push
-			if !ok {
-				break
-			}
-			a.head = new(queueNode)
-			a.tail = a.head
-			a.head.data = f
-			a.Unlock()
-			closed := false
-			length := 1
-			for a.head != nil && !closed {
-				a.Lock()
-				t := a.head.data
-				a.head = a.head.next
+			for len(a.list) > 0 {
 				select {
-				case a.length <- length:
-				default:
-				}
-				select {
-				case a.pop <- t:
-					length--
-				case i, ok := <- a.push:
+				case in, ok := <- a.push:
 					if ok {
-						a.tail.next = new(queueNode)
-						a.tail = a.tail.next
-						a.tail.data = i
-						length++
+						a.appendToList(in)
 					} else {
-						a.pop <- t
-						break OUTER
+						goto DRAIN
 					}
+				case a.pop <- a.list[0]:
+					a.list = a.list[1:]
 				}
-				a.Unlock()
 			}
 		}
-		for a.head != nil {
-			a.Lock()
-			t := a.head.data
-			a.head = a.head.next
-			a.pop <- t
-			a.Unlock()
+		DRAIN: for len(a.list) > 0 {
+			a.pop <- a.list[0]
+			a.list = a.list[1:]
 		}
 		close(a.pop)
 	}()
@@ -112,36 +129,65 @@ func NewQueue() *Queue {
 }
 
 func (a *Queue) Pop() (i json.Marshaler, ok bool) {
+	a.Lock()
+	defer a.Unlock()
 	i, ok = <- a.pop
 	return
 }
 
-func (a *Queue) Push(i json.Marshaler) {
+func (a *Queue) Push(i json.Marshaler) error {
+	a.Lock()
+	defer a.Unlock()
+	// log.Println("push", i)
+	return a.pushBase(inq{i, nil})
+}
+
+func (a *Queue) PushWait(i json.Marshaler) error {
+	a.Lock()
+	defer a.Unlock()
+	c := make(chan struct{})
+	defer func(){ <- c }()
+	return a.pushBase(inq{i, c})
+}
+
+func (a *Queue) pushBase(i inq) error {
+	if a.closed {
+		return ClosedError{a}
+	}
 	a.push <- i
+	log.Println("pushed", i)
+	return nil
 }
 
 func (a *Queue) MarshalJSON() ([]byte, error) {
-	a.Lock()
+	a.RLock()
+	defer a.RUnlock()
 	out := []byte{'['}
-	comma := ''
-	for h := a.head; h != nil; {
-		b, err := h.data.MarshalJSON()
+	comma := ""
+	for _, h := range a.list {
+		out = append(out, []byte(comma)...)
+		b, err := h.MarshalJSON()
 		if err != nil {
 			return nil, err
 		}
-		out = append(out, b..., comma)
-		comma = ','
+		out = append(out, b...)
+		comma = ","
 	}
 	out = append(out, ']')
-	return b, nil
+	return out, nil
 }
 
 func (a *Queue) Stop() {
-	close(a.push)
+	if !a.closed {
+		a.closed = true
+		close(a.push)
+	}
 }
 
 func (a *Queue) Len() int {
-	return <- a.length
+	a.RLock()
+	defer a.RUnlock()
+	return len(a.list)
 }
 
 type Corpus interface {
--- queues.go
+++ queues.go
@@ -1,11 +1,43 @@
 package main
 
+import "omnia"
+
 var queues = make(map[string]*Queue)
 var queueProcs = make(map[string][]chan bool)
 
-func init() {
-	queues["upload"] = NewQueue() // type AudioFile
-	queues["request"] = NewQueue() // type PostAudioFile
-	queues["add"] = NewQueue() // type CorpusAudioFile
-	
+func StartQueues() {
+	omnia.ParseConf()
+	bufsize := omnia.Conf["bufsize"]
+	queues["upload"] = omnia.NewQueue(bufsize) // type AudioFile
+	queues["request"] = omnia.NewQueue(bufsize) // type PostAudioFile
+	queues["add"] = omnia.NewQueue(bufsize) // type CorpusAudioFile	
+}
+
+type q struct{}
+
+func AudioFileProcessor(quitchan chan q) {
+	go func() {
+		for {
+			select {
+			case <- quitchan:
+				return
+			}
+			q, ok := queues["upload"].Pop()
+			if !ok {
+				return
+			}
+			af := q.(AudioFile) // panic is good here
+			// submit to echonest
+			trackid, tatums := AnalyzeFile(af)
+			if af.Corpus {
+				at := FillAudioTatums(af, trackid, tatums)
+				queues["add"].Push(at)
+			}
+			if af.Request {
+				paf := omnia.PostAudioFile{TrackId:trackid, InfoUrl:af.InfoUrl, Tatums:tatums}
+				queues["request"].Push(paf)
+			}
+		}
+	}()
+	return c
 }
